0c06a7c5579d6cd388db449c584f726480acab8f
rohrig <richard.t.rohrig@gmail.com>
2024-03-23 23:09:26 +0100

    feat: init

---
+++ mod.ts
@@ -0,0 +1,8 @@
+/**
+ * Roar function
+ * @param phrase - The phrase to roar
+ * @returns The roared phrase
+ */
+export function roar(phrase: string): string {
+  return `${phrase.toUpperCase()} !!! ROAR!`
+}
