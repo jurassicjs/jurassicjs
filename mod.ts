a3a45fef9acf72f0eced1f2bad5def1fcb6e0157
rohrig <richard.t.rohrig@gmail.com>
2024-03-23 23:15:42 +0100

    feat: add pub action and coo

--- mod.ts
+++ mod.ts
@@ -6,3 +6,12 @@
 export function roar(phrase: string): string {
   return `${phrase.toUpperCase()} !!! ROAR!`
 }
+
+/**
+ * Coo function
+ * @param phrase - The phrase to coo
+ * @returns The coo phrase
+ */
+export function coo(phrase: string): string {
+  return `${phrase.toLowerCase()} coo`
+}
